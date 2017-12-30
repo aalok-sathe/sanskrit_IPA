8ae73a1aea24f2bd1a1629a7a9afd26da6e0ebe0
Aalok Sathe <aalok.sathe@gmail.com>
2017-12-30 23:42:16 +0530

    added initial transcription rule(s) and vowels set

--- Dev_IPA_RuleBasedTranscription.py
+++ Dev_IPA_RuleBasedTranscription.py
@@ -6,7 +6,15 @@ def transcribe(string):
 	"""Transcribes the input Sanskrit devanagari text into IPA"""
 	transText = ""
 	for char in string:
-		pass #pending
+		if not charInRange(char):
+			#raise Exception("Character %s out of range."%str(char))
+			pass
+		if (char == '्') or (str(char) in vowels) :
+			#if transText[len(transText)-1] == 'ə':
+			transText = transText[:len(transText)-1]
+		transText += str(mapping.get(char, '?'))
+		print(transText + '\n')
+	return transText
 	
 	
 def charInRange(c):
@@ -14,7 +22,7 @@ def charInRange(c):
 	o = ord(c)
 	lower = int('0x900', 16)
 	upper = int('0x97f', 16)
-	"""List of Devanagari characters that are not used in Sanskrit-Dev orthography"""
+	"""Set of Devanagari characters that are not used in Sanskrit-Dev orthography"""
 	exclude = {'0x900', '0x904', '0x90e', '0x912', '0x929', '0x931', '0x934', '0x93a', '0x93b', '0x93c', '0x946', '0x94e', '0x94f', '0x94a', '0x951', '0x952', '0x953', '0x954', '0x955', '0x956', '0x957', '0x958', '0x959', '0x95a', '0x95b', '0x95c', '0x95d', '0x95e', '0x95f', '0x973', '0x974', '0x975', '0x976', '0x977', '0x978', '0x979', '0x97a', '0x97b', '0x97c', '0x97f', '0x97d', '0x97e', '0x970', '0x971'}
 	return (o >= lower and o <= upper) and not (hex(o) in exclude)
 	
@@ -42,4 +50,15 @@ mapping = {
 	'य' : 'jə', 'र' : 'ɹə', 'ल' : 'lə', 'व' : 'ʋə', 'श' : 'ɕə',
 	'ष' : 'ʂə', 'स' : 'sə', 'ह' : 'ɦə', 'ळ' : 'ɭə',
 	'क्ष' : 'kʂə', 'ज्ञ' : 'd͡ʒɲə', 'त्र' : 't̪ɹə',
+	'ऽ' : 'ː', '्' : '', ' ' : ' ',
+}
+
+vowels = {
+	'ा' : 'ɑ',
+	'ि' : 'i',	'ी' : 'iː',
+	'ु' : 'u', 'ू' : 'uː',
+	'ृ' : 'ɹ̩', 'ॄ' : 'ɹ̩ː', 'ॢ' : 'l̩',
+	'े' : 'eː', 'ै' : 'əi',
+	'ो' : 'oː', 'ाै' : 'əu',
+	'ं' : 'əm', 'ः' : 'əh',
 }
