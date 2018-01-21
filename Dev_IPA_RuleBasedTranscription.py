31467f973e81abc7bafec199ed7b5ab0264d7480
Aalok Sathe <aalok.sathe@gmail.com>
2018-01-21 15:33:02 -0500

    updated stress placement using indexOf(".")

--- Dev_IPA_RuleBasedTranscription.py
+++ Dev_IPA_RuleBasedTranscription.py
@@ -24,73 +24,8 @@ def transcribe(string):
 		International Phonetic Alphabet"""
 		
 	transText = str()
+	string = 'अ' + string
 	
-################################################
-########	Old block for syllabification	####
-########	[Disabled]		####################
-################################################	
-
-	vowelB = vowelA = 0	
-
-	charIndex = 0
-	
-############	Temporarily disabled.	########
-
-	while False:
-		try :
-			c = string[charIndex]
-		except Exception :
-			break
-	
-		if not charInRange(string[charIndex]):
-			#raise Exception("Character %s out of range."%str(char))
-			pass
-
-		if (string[charIndex] in mapping['VOWELS']) or (charIndex+1 < len(string) and string[charIndex+1] != "्" and not string[charIndex+1] in mapping['VOWELS']) :
-			vowelB = vowelA
-			vowelA = charIndex
-			splitStr = string[vowelB+1:vowelA]
-			print(splitStr)
-			splitLen = 0
-			numHalant = 0
-			for counter in range(len(splitStr)) :
-				if not splitStr[counter] == '्' :
-					splitLen += 1
-				else :
-					numHalant += 1
-					
-			if splitLen == 1 :
-				print(1)
-				string = string[:vowelB+1] + "." + string[vowelB+1:]
-			elif splitLen == 2 :
-				print(2)
-				string = string[:vowelB+2+1] + "." + string[vowelB+2+1:]
-			elif splitLen == 3 :
-				print(3)
-				if string[vowelA-2] in {'र', 'य'} :
-					string = string[:vowelB+2+1] + "." + string[vowelB+2+1:]
-				elif string[vowelA-4] in stops and string[vowelA-6] in stops :
-					string = string[:vowelB+2+1] + "." + string[vowelB+2+1:]
-				else :
-					string = string[:vowelB+2+2+1] + "." + string[vowelB+2+2+1:]
-			elif splitLen :
-				print(4)
-				if string[vowelA-2] in {'र', 'य'} :
-					string = string[:vowelA-2-2] + "." + string[vowelA-2-2:]
-				else :
-					minSonorous = vowelB + 1
-					minSonority = 8
-					for i in range(splitLen,2) :
-						if sonority.get(string[vowelB+1+i],100) < minSonority  :
-							minSonorous = vowelB+1+i
-							minSonority = sonority.get(string[minSonorous])
-					string = string[:vowelB+minSonorous+1] + "." + string[vowelB+minSonorous+1:]
-			if splitLen :
-				charIndex += 1
-			
-		charIndex += 1
-		print(string)
-
 ################################################
 ########	Actual transcription block	########
 ################################################	
@@ -101,12 +36,15 @@ def transcribe(string):
 		if (string[charIndex] == '्') or (str(string[charIndex]) in mapping['VOWELS']) :
 			"""Decision block to delete preceding schwa (ə) if the current char
 			is either a vowel modifier or a 'halant' indicator."""
-			try:
-				transText = transText[:len(transText)-1]
-				if transText[len(transText)-1] == "ə" :
+			if not string[charIndex] in {'ः' : 'əx', 'ं' : 'əm',} :
+				try:
 					transText = transText[:len(transText)-1]
-			except IndexError:
-				print("Warning: You are attempting to transcribe a stand-alone diacritical mark.\n")
+					if transText[len(transText)-1] == "ə" :
+						transText = transText[:len(transText)-1]
+				except IndexError:
+					print("Warning: You are attempting to transcribe a stand-alone diacritical mark.\n")
+			else :
+				pass
 
 		if prevChar == 1 :
 			"""Check to see if the previous character had an 'anuswar',
@@ -154,13 +92,14 @@ def transcribe(string):
 			else : prevChar = 0
 				
 		transText += str(mapping.get(string[charIndex], string[charIndex]))
-		#print(transText + '\n')
+		
+	#print(transText + '\n')	# for debugging
 		
 ####################################################
 ########	Modified string for syllabification	####
 ########	and stress assignment	################
 		
-		altTransText = '.'
+	altTransText = '.'
 		
 ####################################################
 ########	Begin new block for syllabification	####
@@ -170,10 +109,10 @@ def transcribe(string):
 	vowelB = getNextVowelIndex(transText, -1)['index']
 	length = lastLength = getNextVowelIndex(transText, -1)['length']
 	vowelB += lastLength
-	
+	#print (vowelB,vowelA,lastLength)
 	prevSyllBreak = 0
 	
-	while True:
+	while vowelA != None and vowelB != None :
 		vowelA = getNextVowelIndex(transText, vowelB)['index']
 		length = getNextVowelIndex(transText, vowelB)['length']
 		clusterComponents = []
@@ -188,16 +127,16 @@ def transcribe(string):
 		cluster = transText[vowelB : vowelA]
 		#print(cluster)
 		for charIndex in range(len(cluster)) :
-			if not cluster[charIndex] in "'͡ʃʰʱː̪" :
+			if not cluster[charIndex] in "'͡ʃʰʱː̪ʒ" :
 				clusterComponents.append(cluster[charIndex])
 			else :
 				clusterComponents[len(clusterComponents)-1] += cluster[charIndex]
 		
-		print (transText[vowelB-lastLength:vowelB])
+		#print (transText[vowelB-lastLength:vowelB])
 		
 		altTransText += transText[vowelB-lastLength:vowelB]
 		
-		print (clusterComponents)
+		#print (clusterComponents)
 		
 		if len(clusterComponents) == 1 :
 			# do not mark stress because len() == 1
@@ -207,7 +146,14 @@ def transcribe(string):
 		elif len(clusterComponents) == 2 :
 			if len(altTransText) and clusterComponents[0] not in {'ŋ', 'ɲ', 'ɳ', 'n', 'm',} :
 				if altTransText[len(altTransText)-1] != '̃' and (lastLength == 1 or altTransText[len(altTransText)-1] == '̩') :
-					altTransText = altTransText[:prevSyllBreak+1] + 'ˈ' + altTransText[1+prevSyllBreak:]
+					#altTransText = altTransText[:prevSyllBreak+1] + 'ˈ' + altTransText[1+prevSyllBreak:]
+					for i in range(1,10) :
+						try :
+							periodIndex = len(altTransText)-i+altTransText[-i:].index('.')
+							altTransText = altTransText[:periodIndex+1] + 'ˈ' + altTransText[periodIndex+1:]
+							break
+						except ValueError :
+							pass
 			#^mark stress
 			prevSyllBreak = len(altTransText) + 1 + int(' ' in clusterComponents)
 			altTransText += clusterComponents[0] + '.' + clusterComponents[1]
@@ -215,28 +161,56 @@ def transcribe(string):
 			if (clusterComponents[2] in {'j', 'ɹ',}) :
 				if len(altTransText) and clusterComponents[0] not in {'ŋ', 'ɲ', 'ɳ', 'n', 'm',} :
 					if altTransText[len(altTransText)-1] != '̃' and (lastLength == 1 or altTransText[len(altTransText)-1] == '̩') :
-						altTransText = altTransText[:prevSyllBreak+1] + 'ˈ' + altTransText[1+prevSyllBreak:]
+						#altTransText = altTransText[:prevSyllBreak+1] + 'ˈ' + altTransText[1+prevSyllBreak:]
+						for i in range(1,10) :
+							try :
+								periodIndex = len(altTransText)-i+altTransText[-i:].index('.')
+								altTransText = altTransText[:periodIndex+1] + 'ˈ' + altTransText[periodIndex+1:]
+								break
+							except ValueError :
+								pass
 				#^mark stress
 				prevSyllBreak = len(altTransText) + 1 + int(' ' in clusterComponents)
 				altTransText += clusterComponents[0] + '.' + clusterComponents[1] + clusterComponents[2]
 			elif (clusterComponents[0] in stops.values()) and (clusterComponents[1] in stops.values()) :
 				if len(altTransText) and clusterComponents[0] not in {'ŋ', 'ɲ', 'ɳ', 'n', 'm',} :
 					if altTransText[len(altTransText)-1] != '̃' and (lastLength == 1 or altTransText[len(altTransText)-1] == '̩') :
-						altTransText = altTransText[:prevSyllBreak+1] + 'ˈ' + altTransText[1+prevSyllBreak:]
+						#altTransText = altTransText[:prevSyllBreak+1] + 'ˈ' + altTransText[1+prevSyllBreak:]
+						for i in range(1,10) :
+							try :
+								periodIndex = len(altTransText)-i+altTransText[-i:].index('.')
+								altTransText = altTransText[:periodIndex+1] + 'ˈ' + altTransText[periodIndex+1:]
+								break
+							except ValueError :
+								pass
 				#^mark stress
 				prevSyllBreak = len(altTransText) + 1 + int(' ' in clusterComponents)
 				altTransText += clusterComponents[0] + '.' + clusterComponents[1] + clusterComponents[2]
 			else :
 				if len(altTransText) and clusterComponents[0] not in {'ŋ', 'ɲ', 'ɳ', 'n', 'm',} :
 					if altTransText[len(altTransText)-1] != '̃' and (lastLength == 1 or altTransText[len(altTransText)-1] == '̩') :
-						altTransText = altTransText[:prevSyllBreak+1] + 'ˈ' + altTransText[1+prevSyllBreak:]
+						#altTransText = altTransText[:prevSyllBreak+1] + 'ˈ' + altTransText[1+prevSyllBreak:]
+						for i in range(1,10) :
+							try :
+								periodIndex = len(altTransText)-i+altTransText[-i:].index('.')
+								altTransText = altTransText[:periodIndex+1] + 'ˈ' + altTransText[periodIndex+1:]
+								break
+							except ValueError :
+								pass
 				#^mark stress
 				prevSyllBreak = len(altTransText) + 2 + int(' ' in clusterComponents)
 				altTransText += clusterComponents[0] + clusterComponents[1] + '.' + clusterComponents[2]				
 		else :
 			if len(altTransText) and clusterComponents[0] not in {'ŋ', 'ɲ', 'ɳ', 'n', 'm',} :
 				if altTransText[len(altTransText)-1] != '̃' and (lastLength == 1 or altTransText[len(altTransText)-1] == '̩') :
-					altTransText = altTransText[:prevSyllBreak+1] + 'ˈ' + altTransText[1+prevSyllBreak:]
+					#altTransText = altTransText[:prevSyllBreak+1] + 'ˈ' + altTransText[1+prevSyllBreak:]
+					for i in range(1,10) :
+						try :
+							periodIndex = len(altTransText)-i+altTransText[-i:].index('.')
+							altTransText = altTransText[:periodIndex+1] + 'ˈ' + altTransText[periodIndex+1:]
+							break
+						except ValueError :
+							pass
 			for i in range(len(clusterComponents)) : altTransText += clusterComponents[i]
 			
 		vowelB = vowelA+length
@@ -245,15 +219,12 @@ def transcribe(string):
 	altTransText += transText[vowelB-lastLength:]
 	
 ####################################################
-########	Begin block for stress assignment	####
-####################################################
-
-	
+########	End block for stress assignment	########
 ####################################################
 ########	Return transcribed text	################
 ####################################################	
 		
-	return altTransText[1:] #+ "\n" + transText
+	return altTransText[3:] #+ "\n" + transText
 	
 ####################################################
 ########	Helper function to find position of	####
@@ -262,6 +233,9 @@ def transcribe(string):
 
 def getNextVowelIndex(ipaString, currentIndex) :
 	"""Returns index of the first vowel after, excluding, current index"""
+	if currentIndex <= 0 :
+		pass
+		#return {'index' : 0, 'length' : 1}
 	for index in range(1+currentIndex,len(ipaString)) :
 		for iterator in range(5) :
 			if ipaString[index:index+1+4-iterator] in set(vowels.values()).union({None}) - {'əm', 'əh'} :
@@ -302,8 +276,8 @@ mapping = {
 	'ऐ' : 'əi', 'ै' : 'əi',
 	'ओ' : 'oː', 'ो' : 'oː',
 	'अाै' : 'əu', 'ाै' : 'əu',
-	'अं' : 'əm', 'ं' : 'əm',
-	'अः' : 'əx', 'ः' : 'əx',
+	'अं' : 'əm', 'ं' : 'm',
+	'अः' : 'əx', 'ः' : 'x',
 	'VOWELS' : {
 		'ा' : 'ɑː',
 		'ि' : 'i',	'ी' : 'iː',
@@ -367,21 +341,22 @@ longVowels = {
 }
 
 stops = {
-	'क' : 'k', 'ख' : 'kʰ', 'ग' : 'g', 'घ' : 'gʰ', #'ङ' : 'ŋə',
-	'च' : 't͡ʃ', 'छ' : 't͡ʃʰ', 'ज' : 'd͡ʒ', 'झ' : 'd͡ʒʱ', #'ञ' : 'ɲə',
-	'ट' : 'ʈ', 'ठ' : 'ʈʰ', 'ड' : 'ɖ', 'ढ' : 'ɖʰ', #'ण' : 'ɳə',
-	'त' : 't̪', 'थ' : 't̪ʰ', 'द' : 'd̪', 'ध' : 'd̪ʰ', #'न' : 'nə',
-	'प' : 'p', 'फ' : 'pʰ', 'ब' : 'b', 'भ' : 'bʱ', #'म' : 'mə',
+	'क' : 'k', 'ख' : 'kʰ', 'ग' : 'g', 'घ' : 'gʰ',
+	'च' : 't͡ʃ', 'छ' : 't͡ʃʰ', 'ज' : 'd͡ʒ', 'झ' : 'd͡ʒʱ',
+	'ट' : 'ʈ', 'ठ' : 'ʈʰ', 'ड' : 'ɖ', 'ढ' : 'ɖʰ',
+	'त' : 't̪', 'थ' : 't̪ʰ', 'द' : 'd̪', 'ध' : 'd̪ʰ',
+	'प' : 'p', 'फ' : 'pʰ', 'ब' : 'b', 'भ' : 'bʱ',
 }
 
 sonority = {
-	'क' : 1, 'च' : 1, 'ट' : 1, 'त' : 1, 'प' : 1,
-	'ख' : 2, 'छ' : 2, 'ठ' : 2, 'थ' : 2, 'फ' : 2,
-	'ग' : 3, 'ज' : 3, 'ड' : 3, 'द' : 3, 'ब' : 3,
-	'घ' : 4, 'झ' : 4, 'ढ' : 4, 'ध' : 4, 'भ' : 4,
-	'ङ' : 5, 'ञ' : 5, 'ण' : 5, 'न' : 5, 'म' : 5,
+	'k' : 1, 't͡ʃ' : 1, 'ʈ' : 1, 't̪' : 1, 'p' : 1,
+	'kʰ' : 2, 't͡ʃʰ' : 2, 'ʈʰ' : 2, 't̪ʰ' : 2, 'pʰ' : 2,
+	'g' : 3, 'd͡ʒ' : 3, 'ɖ' : 3, 'd̪' : 3, 'b' : 3,
+	'gʰ' : 4, 'd͡ʒʱ' : 4, 'ɖʰ' : 4, 'd̪ʰ' : 4, 'bʱ' : 4,
+	'ŋ' : 5, 'ɲ' : 5, 'ɳ' : 5, 'n' : 5, 'm' : 5,
 	'श' : 6, 'ष' : 6, 'स' : 6, 'ह' : 6, 'ळ' : 6,
 	'य' : 7, 'र' : 7, 'ल' : 7, 'व' : 7,
 	# vowels : 8,
 }
+
 
